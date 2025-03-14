24874eee439b3436e98f337f4b55970c6b9416e2
Christian Cesar <christiancnp@gmail.com>
2025-03-14 17:23:53 -0400

    feat: added address prisma repository

---
+++ src/database/repositories/prisma/address-repository-prisma.ts
@@ -0,0 +1,27 @@
+import { CreateAddress } from "@database/repositories/dtos/create-address-dto.js";
+import { FindAddressById } from "@database/repositories/dtos/find-address-by-id-dto.js";
+import { Address } from "@entities/address.js";
+import { prisma } from "@src/database/providers/prisma/prisma.js";
+import { AddressMapper } from "./mappers/address-mapper.js";
+
+export class AddressesRepositoryPrisma {
+  async createAddress(data: CreateAddress): Promise<Address> {
+    const address = await prisma.address.create({
+      data,
+    });
+
+    return AddressMapper.toEntity(address);
+  }
+
+  async findAddressById({
+    addressId,
+  }: FindAddressById): Promise<Address | undefined> {
+    const address = await prisma.address.findUnique({
+      where: {
+        id: addressId,
+      },
+    });
+
+    return address ? AddressMapper.toEntity(address) : undefined;
+  }
+}
